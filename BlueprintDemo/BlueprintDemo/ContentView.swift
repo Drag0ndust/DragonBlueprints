ce8f3a8909e993185c86d3faad0ecbd0780cc209
Thimo Bess <thimo.bess@gmail.com>
2024-07-07 18:04:01 +0200

    demo: add demo example project

---
+++ BlueprintDemo/BlueprintDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueprintDemo
+//
+//  Created by Drag0ndust on 07.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
